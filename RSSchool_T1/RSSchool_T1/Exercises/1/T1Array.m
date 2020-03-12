38a8782732f0e4c7473fd85058b64310eeed0621
Артем Холодок <holod@MacBook-Pro-Artem.local>
2020-03-12 10:51:05 +0300

    init task 1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,41 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *preResult = [[NSMutableArray alloc] init];
+    if ([sadArray  isEqual: @[]]) {
+        return @[];
+    }
+    if ([sadArray count] < 3) {
+        return sadArray;
+    }
+    [preResult addObject: [sadArray objectAtIndex: 0]];
+    for (int i = 1; i < [sadArray count] - 1; i++) {
+        NSNumber *curentNum = [sadArray objectAtIndex: i];
+        NSNumber *preNum = [sadArray objectAtIndex: i - 1];
+        NSNumber *nextNum = [sadArray objectAtIndex: i + 1];
+        if ([curentNum intValue] < [nextNum intValue] + [preNum intValue]) {
+            [preResult addObject: curentNum];
+        }
+    }
+    [preResult addObject: [sadArray objectAtIndex: [sadArray count] - 1]];
+    if ([self isHappy: [preResult copy]]) {
+        return [preResult copy];
+    } else {
+        preResult = [self convertToHappy: [preResult copy]];
+    }
+    return [preResult copy];
+}
+
+- (BOOL)isHappy:(NSArray *)array {
+    for (int i = 1; i < [array count] - 1; i++) {
+           NSNumber *curentNum = [array objectAtIndex: i];
+           NSNumber *preNum = [array objectAtIndex: i - 1];
+           NSNumber *nextNum = [array objectAtIndex: i + 1];
+           if ([curentNum intValue] > [nextNum intValue] + [preNum intValue]) {
+               return false;
+           }
+       }
+    return true;
 }
 
 @end
