a566bc57970d3754309437be6a89c5dcd3eba04b
Артем Холодок <holod@MacBook-Pro-Artem.local>
2020-03-15 20:07:52 +0300

    init task 3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,6 +4,14 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
+    NSMutableArray<NSNumber*> *mutableBill = [[NSMutableArray alloc] initWithArray: bill];
+    [mutableBill removeObjectAtIndex: index];
+    NSNumber * sumArray = [mutableBill valueForKeyPath:@"@sum.self"];
+    if ([sumArray intValue] / 2 == [sum intValue]) {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat:@"%d", ([sum intValue] - [sumArray intValue] / 2)];
+    }
     return @"";
 }
 
